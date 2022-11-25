67abdf95eafb541119ac10aeda3ef4ad5d128c62
Hamdan Yuwafi <yuwafi.hamdan365@gmail.com>
2022-11-26 04:36:24 +0700

    feat(stoplight): add stoplight api-spec viewer to factory and routers

--- src/app/app.constructor.go
+++ src/app/app.constructor.go
@@ -1,7 +1,13 @@
 package app
 
-type Factory struct{}
+import "skk-backend-todolist/src/app/stoplight"
+
+type Factory struct {
+	Stoplight stoplight.Handler
+}
 
 func Construct() Factory {
-	return Factory{}
+	return Factory{
+		Stoplight: stoplight.Construct(),
+	}
 }
--- src/routes/web.route.go
+++ src/routes/web.route.go
@@ -1,5 +1,14 @@
 package routes
 
+import (
+	"skk-backend-todolist/src/app"
+)
+
 func (r *Router) ConstructWeb() *Router {
+	var feature = app.Construct()
+
+	r.route.GET("/docs", feature.Stoplight.GetAPISpec)
+	r.route.GET("/assets/apispec.json", feature.Stoplight.ServeDocsFile)
+
 	return r
 }
